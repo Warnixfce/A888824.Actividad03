7a21dafa23dbb514bf88d7ec1ce23050ce329c8b
MEfce <marinacespinosa@yahoo.com.ar>
2021-05-10 12:06:28 -0300

    Cambios

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace A888824.Actividad03
             //               02|03/04/2020|4000|200
             //               03|06/15/2020|6000|300
             // Considerar que mi pc está en INGLÉS (dejo el contenido con ese formato porque entiendo que tu pc también está en inglés).
-            // Por favor si podes poner los archivos en la ruta "bin\Debug" para probarlo, te lo agradecería;
+            // Por favor si podes poner los archivos en la ruta "bin\Debug" para probarlo (porque entiendo que los archivos ya existirían), te lo agradecería;
             // no es mi intención tener mal la actividad por este motivo.
             //                     --------------------------------
 
